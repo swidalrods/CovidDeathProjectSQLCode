96e58dbd2f0fae2352961b4afb1cf120e3ab8b27
swidalrods <102979980+swidalrods@users.noreply.github.com>
2022-04-05 13:27:06 -0500

    Add files via upload

---
+++ Tableau Project Quries.sql
@@ -0,0 +1,47 @@
+/*
+Queries used for Tableau Project
+*/
+
+-- 1. Globle NUmbers
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+-- TotalDeath Number Per Continent.
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.looking at countries with hightest infection rate campare to population.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.looking at time series of percentpopulation infected by location 
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
+
+
+
+
+
